f96af9f673fbc50be15a5f3ca3cb41f52eff91b2
kamel-sarhiri <kamel.sarhiri@laplateforme.io>
2022-12-13 17:59:42 +0100

    suppresion mauvais fichier

--- livre-or-main/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Connexion à la base de données en local
-
-$connect = new mysqli('localhost', 'root', '', 'livreor');
-
-// Connexion à la base de données en ligne
-
-// $connect = new mysqli('localhost', 'adminbdd', 'basededonnees', 'thomas-spinec_livreor')
-?>
