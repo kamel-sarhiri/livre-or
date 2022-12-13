1482823fed7220dd42a232fd9f945e9e555a89f8
kamel-sarhiri <114387782+kamel-sarhiri@users.noreply.github.com>
2022-12-13 17:52:26 +0100

    ajout fichier php
    site fonctionnel

---
+++ livre-or-main/index.php
@@ -0,0 +1,23 @@
+    <!-- header des pages -->
+    <?php
+        include 'header.php';
+    ?>
+
+    <!-- contenu de la page -->
+    <main>
+        <div class="center">
+            <h1>Bienvenue sur le livre d'or de Kamel</h1>
+            <p>Vous pouvez laisser un message sur le livre d'or</p>
+            <p>Vous devez être connecté pour laisser un commentaire</p>
+            <img src="img/photo profil.jpg" alt="photo develloper">
+            
+
+        </div>
+    </main>
+
+    <!-- footer des pages -->
+    <?php
+        include 'footer.php';
+    ?>
+</body>
+</html>
